f9844f90a09cb91b113db2ff046a25342f4e9bfa
Scott Dudley <scott@sustainableharvest.com>
2015-03-24 09:22:25 -0700

    Allow discard for Contacts from Campaigns

--- app/models/entities/campaign.rb
+++ app/models/entities/campaign.rb
@@ -94,7 +94,7 @@ class Campaign < ActiveRecord::Base
   def discard!(attachment)
     if attachment.is_a?(Task)
       attachment.update_attribute(:asset, nil)
-    elsif attachment.is_a?(Account)
+    elsif attachment.is_a?(Account) || attachment.is_a?(Contact)
       attachment.campaigns.delete(self)
     else # Leads, Opportunities
       attachment.send("decrement_#{attachment.class.name.tableize}_count")
