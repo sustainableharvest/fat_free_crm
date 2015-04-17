e98173a61e993bd8a6170f4da4238f76e7c787ae
Scott Dudley <scott@sustainableharvest.com>
2015-04-16 17:12:47 -0700

    Add all taggings to Tag show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,6 +8,15 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find(params[:id])
+    @taggings = get_taggings(@tag)
+  end
+
+  def get_taggings(tag)
+    tags = []
+    tag.taggings.each do |tagging|
+      tags << tagging.taggable_type.constantize.find(tagging.taggable_id)
+    end
+    tags
   end
 
 end
