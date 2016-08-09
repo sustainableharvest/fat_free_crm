3389832dca4ab748ebebaeedc2c7edb0c94d277d
Scott Dudley <scott@sustainableharvest.com>
2016-08-09 10:30:25 -0700

    Add import

--- import_script.rb
+++ import_script.rb
@@ -2,7 +2,8 @@ ltc.each do |contact|
   match = Contact.where(email: contact["Email Address"])
   if match.empty?  
     c = Contact.create(first_name: contact["Name"], last_name: contact["Last Name"], email: contact["Email Address"])
-    c.comments << Comment.create(user_id: 1, commentable_type: "Contact", comment: "FROM IMPORT\n\n" + contact["COUNTRY"].to_s + "\n\n " + contact["CONTACT TYPE"].to_s + "\n\n " + contact["Company"].to_s)
+    c.addresses << Address.create(country: contact["COUNTRY"], address_type: "Business")
+    c.comments << Comment.create(user_id: 1, commentable_type: "Contact", comment: "FROM IMPORT\n\nCountry: " + contact["COUNTRY"].to_s + "\n\nType: " + contact["CONTACT TYPE"].to_s + "\n\nCompany: " + contact["Company"].to_s)
     c.tag_list= "LTC2014"
     c.save
   end  
